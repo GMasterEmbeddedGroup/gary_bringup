3b4229c859ff67bb9021377da08688bd7b6910d6
myaki-moe <i@myaki.moe>
2023-02-11 19:58:39 +0800

    feat: add launch file for gary_common

---
+++ launch/gary_common/lifecycle_manager.launch.py
@@ -0,0 +1,52 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import PathJoinSubstitution, PythonExpression, LaunchConfiguration
+
+from launch_ros.actions import LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    robot_type_arg = DeclareLaunchArgument("robot_type")
+
+    lifecycle_manager_params = PathJoinSubstitution(
+        [
+            FindPackageShare("gary_bringup"),
+            "config",
+            LaunchConfiguration("robot_type"),
+            "gary_common",
+            "lifecycle_manager.yaml",
+        ]
+    )
+
+    lifecycle_manager = LoadComposableNodes(
+        target_container='/ComponentManager',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='gary_common',
+                plugin='gary_common::LifecycleManager',
+                parameters=[lifecycle_manager_params],
+            ),
+        ],
+        condition=IfCondition(PythonExpression(
+            ["__import__('os').path.exists(\"",
+             PathJoinSubstitution([
+                 FindPackageShare("gary_bringup"),
+                 "config",
+                 LaunchConfiguration("robot_type"),
+                 "gary_common",
+                 "lifecycle_manager.yaml"
+             ]),
+             "\")"]
+        ))
+    )
+
+    description = [
+        robot_type_arg,
+        lifecycle_manager,
+    ]
+
+    return LaunchDescription(description)
