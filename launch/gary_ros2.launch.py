4ddfcbdfbcb21e69e1c7724968e75a7bcf395ab9
myaki-moe <i@myaki.moe>
2023-03-02 23:30:14 +0800

    reflect: combined and optmized launch action in a single file

--- launch/gary_ros2.launch.py
+++ launch/gary_ros2.launch.py
@@ -1,99 +1,214 @@
-from launch import LaunchDescription
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, PythonExpression
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.conditions import IfCondition
-from launch_ros.actions import Node
+import glob
+import os
+import yaml
 
+from launch import LaunchDescription, LaunchContext
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, FindExecutable
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, LogInfo, TimerAction
+from launch_ros.actions import Node, LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
 from launch_ros.substitutions import FindPackageShare
 
 
-def generate_launch_description():
-    robot_type_arg = DeclareLaunchArgument("robot_type")
-    debug_arg = DeclareLaunchArgument("debug", default_value="False")
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
-    container = Node(
+
+def check_config_file_exist(context: LaunchContext, robot_type: LaunchConfiguration):
+    ret = []
+
+    if not os.path.exists(
+            os.path.join(FindPackageShare("gary_bringup").perform(context), "config", robot_type.perform(context))):
+        config_not_found_msg = bcolors.WARNING + "\n" \
+            "   _____ ____  _   _ ______ _____ _____    _   _  ____ _______    ______ ____  _    _ _   _ _____  \n" \
+            "  / ____/ __ \| \ | |  ____|_   _/ ____|  | \ | |/ __ \__   __|  |  ____/ __ \| |  | | \ | |  __ \ \n" \
+            " | |   | |  | |  \| | |__    | || |  __   |  \| | |  | | | |     | |__ | |  | | |  | |  \| | |  | |\n" \
+            " | |   | |  | | . ` |  __|   | || | |_ |  | . ` | |  | | | |     |  __|| |  | | |  | | . ` | |  | |\n" \
+            " | |___| |__| | |\  | |     _| || |__| |  | |\  | |__| | | |     | |   | |__| | |__| | |\  | |__| |\n" \
+            "  \_____\____/|_| \_|_|    |_____\_____|  |_| \_|\____/  |_|     |_|    \____/ \____/|_| \_|_____/ \n" \
+            + bcolors.ENDC
+        ret.append(LogInfo(msg=config_not_found_msg))
+
+    if not os.path.exists(
+            os.path.join(FindPackageShare("gary_description").perform(context), "urdf", "ros2_control",
+                         robot_type.perform(context) + ".urdf")):
+        urdf_not_found_msg = bcolors.WARNING + "\n" \
+            "  _    _ _____  _____  ______    _   _  ____ _______    ______ ____  _    _ _   _ _____  \n" \
+            " | |  | |  __ \|  __ \|  ____|  | \ | |/ __ \__   __|  |  ____/ __ \| |  | | \ | |  __ \ \n" \
+            " | |  | | |__) | |  | | |__     |  \| | |  | | | |     | |__ | |  | | |  | |  \| | |  | |\n" \
+            " | |  | |  _  /| |  | |  __|    | . ` | |  | | | |     |  __|| |  | | |  | | . ` | |  | |\n" \
+            " | |__| | | \ \| |__| | |       | |\  | |__| | | |     | |   | |__| | |__| | |\  | |__| |\n" \
+            "  \____/|_|  \_\_____/|_|       |_| \_|\____/  |_|     |_|    \____/ \____/|_| \_|_____/ \n" \
+            + bcolors.ENDC
+        ret.append(LogInfo(msg=urdf_not_found_msg))
+
+    gary_logo_msg = bcolors.OKCYAN + "\n" \
+            "   _____              _____   __     __    _____     ____     _____   ___  \n" \
+            "  / ____|     /\     |  __ \  \ \   / /   |  __ \   / __ \   / ____| |__ \ \n" \
+            " | |  __     /  \    | |__) |  \ \_/ /    | |__) | | |  | | | (___      ) |\n" \
+            " | | |_ |   / /\ \   |  _  /    \   /     |  _  /  | |  | |  \___ \    / / \n" \
+            " | |__| |  / ____ \  | | \ \     | |      | | \ \  | |__| |  ____) |  / /_ \n" \
+            "  \_____| /_/    \_\ |_|  \_\    |_|      |_|  \_\  \____/  |_____/  |____|\n" \
+            "                                                                           \n" \
+            + bcolors.ENDC + \
+            "|===============================================================================|\n" \
+            "| This program is distributed in the hope that it will be useful, but WITHOUT   |\n" \
+            "| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS |\n" \
+            "| FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.|\n" \
+            "|===============================================================================|\n"
+
+    ret.append(TimerAction(period=3.0, actions=[LogInfo(msg=gary_logo_msg)]))
+    return ret
+
+
+def glob_nodes(context: LaunchContext, robot_type: LaunchConfiguration):
+    nodes = []
+    # glob all config file with its package name
+    package_dirs = glob.glob(
+        os.path.join(FindPackageShare("gary_bringup").perform(context), "config", robot_type.perform(context), "*"))
+
+    # foreach package
+    for package_dir in package_dirs:
+        if not os.path.isdir(package_dir):
+            continue
+        # glob all config file with its node name
+        config_files_path = glob.glob(os.path.join(package_dir, "*.yaml"))
+        # foreach all config file
+        for config_file_path in config_files_path:
+            # get package_name and config_file without absolute path
+            package_name = package_dir.split("/")[-1]
+            config_file = config_file_path.split("/")[-1][:-5]
+            # exclude ros2_control
+            if not os.path.isfile(config_file_path) or package_name == "ros2_control":
+                continue
+            # load node
+            node = LoadComposableNodes(
+                target_container='/ComponentManager',
+                composable_node_descriptions=[
+                    ComposableNode(
+                        package=package_name,
+                        plugin=package_name + '::' + config_file,
+                        parameters=[config_file_path]
+                    ),
+                ],
+            )
+            nodes.append(node)
+    return nodes
+
+
+def glob_controllers(context: LaunchContext, robot_type: LaunchConfiguration):
+    controllers = []
+
+    package_path = os.path.join(
+        FindPackageShare("gary_bringup").perform(context), "config", robot_type.perform(context), "ros2_control")
+
+    # check ros2_control dir exists
+    if not os.path.isdir(package_path):
+        return []
+
+    config_files_path = glob.glob(os.path.join(package_path, "*.yaml"))
+
+    controller_configs = {'controller_manager': {'ros__parameters': {'update_rate': 1000}}}
+
+    # foreach all controller config
+    for config_file_path in config_files_path:
+        if not os.path.isfile(config_file_path):
+            continue
+
+        with open(config_file_path, 'r') as config_file:
+
+            # append controller config
+            controller_config = yaml.safe_load(config_file)
+            controller_configs.update(controller_config)
+            for controller_name in controller_config:
+                if controller_name == "controller_manager":
+                    continue
+
+                # add controller type to controller manager
+                controller_configs['controller_manager']['ros__parameters'][controller_name] = dict(
+                    {'type': controller_config[controller_name]['ros__parameters']['controller_type']})
+
+                # add controller spawner
+                controllers.append(controller_spawner(controller_name))
+
+    # robot_description
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("gary_description"),
+                    "urdf",
+                    "ros2_control",
+                    LaunchConfiguration("robot_type").perform(context) + ".urdf",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content.perform(context)}
+
+    # save combined controller config
+    with open("/tmp/launch_params_ros2_control.yaml", 'w') as file:
+        yaml.safe_dump(controller_configs, file)
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, "/tmp/launch_params_ros2_control.yaml"],
+        on_exit=lambda _event, _context: glob_controllers(_context, robot_type)
+    )
+
+    controllers.append(controller_manager)
+
+    return controllers
+
+
+def controller_spawner(controller_name: str, delay=0.0, condition=1):
+    if condition == 1:
+        return Node(
+            package="controller_manager",
+            executable="spawner.py",
+            arguments=[controller_name,
+                       "--controller-manager", "/controller_manager",
+                       ],
+            on_exit=lambda event, context: TimerAction(
+                period=delay,
+                actions=[LogInfo(msg="{} spawn failed, retry in {} seconds".format(controller_name.strip(), delay)),
+                         controller_spawner(controller_name, delay + 1.0, event.returncode)],
+            )
+        )
+    else:
+        return LogInfo(msg="{} spawn successfully".format(controller_name.strip()))
+
+
+def generate_launch_description():
+    description = []
+
+    robot_type_arg = DeclareLaunchArgument("robot_type")
+    description.append(robot_type_arg)
+
+    config_file_check = OpaqueFunction(function=check_config_file_exist, args=[LaunchConfiguration("robot_type")])
+    description.append(config_file_check)
+
+    component_container = Node(
         package='rclcpp_components',
         executable='component_container_mt',
     )
+    description.append(component_container)
 
-    gary_common_description = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gary_bringup'),
-                "launch",
-                "gary_common",
-                'gary_common.launch.py',
-            ])
-        ]), launch_arguments={"robot_type": LaunchConfiguration("robot_type")}.items()
-    )
+    nodes = OpaqueFunction(function=glob_nodes, args=[LaunchConfiguration("robot_type")])
+    description.append(nodes)
 
-    gary_can_description = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gary_bringup'),
-                "launch",
-                "gary_can",
-                'gary_can.launch.py',
-            ])
-        ]), launch_arguments={"robot_type": LaunchConfiguration("robot_type")}.items()
-    )
-
-    gary_serial_description = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gary_bringup'),
-                "launch",
-                "gary_serial",
-                'gary_serial.launch.py',
-            ])
-        ]), launch_arguments={"robot_type": LaunchConfiguration("robot_type")}.items()
-    )
-
-    gary_chassis_description = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gary_bringup'),
-                "launch",
-                "gary_chassis",
-                'gary_chassis.launch.py',
-            ])
-        ]), launch_arguments={"robot_type": LaunchConfiguration("robot_type")}.items()
-    )
-
-    ros2_control_description = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gary_bringup'),
-                "launch",
-                "ros2_control",
-                'ros2_control.launch.py',
-            ])
-        ]), launch_arguments={"robot_type": LaunchConfiguration("robot_type")}.items()
-    )
-
-    debug_description = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gary_bringup'),
-                "launch",
-                "debug.launch.py",
-            ])
-        ]),
-        launch_arguments={"robot_type": LaunchConfiguration("robot_type")}.items(),
-        condition=IfCondition(PythonExpression([LaunchConfiguration("debug"), "== True"]))
-    )
-
-    description = [
-        debug_arg,
-        robot_type_arg,
-        container,
-        gary_common_description,
-        gary_can_description,
-        gary_serial_description,
-        gary_chassis_description,
-        ros2_control_description,
-        debug_description,
-    ]
+    controllers = OpaqueFunction(function=glob_controllers, args=[LaunchConfiguration("robot_type")])
+    description.append(controllers)
 
     return LaunchDescription(description)
